9e6f01777cd858177454f72b4cf886e94c60ae28
Denys Boichuk <boichuk.db@gmail.com>
2026-03-27 22:37:44 +0100

    fix(api): skip 4xx HttpExceptions in Sentry filter to reduce noise

--- apps/api/src/filters/sentry-exception.filter.ts
+++ apps/api/src/filters/sentry-exception.filter.ts
@@ -1,11 +1,17 @@
-import { ArgumentsHost, Catch } from '@nestjs/common';
+import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import * as Sentry from '@sentry/nestjs';
 
 @Catch()
 export class SentryExceptionFilter extends BaseExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost): void {
-    Sentry.captureException(exception);
+    const isExpectedError =
+      exception instanceof HttpException && exception.getStatus() < 500;
+
+    if (!isExpectedError) {
+      Sentry.captureException(exception);
+    }
+
     super.catch(exception, host);
   }
 }
